407a9ec4fd66ca7249b21fe334b733d90c751add
nauqs <aquindsa10@alumnes.ub.edu>
2023-03-05 11:49:06 +0000

    Revise PPO implementation using benchmark

--- first-steps/models.py
+++ first-steps/models.py
@@ -4,8 +4,58 @@ import torch.nn as nn
 _half_log_2pi_ = 0.5*torch.log(torch.tensor(2*torch.pi))
 
 class ActorNet(nn.Module):
-    def __init__(self, state_dim, action_dim, hidden_dim=(64, 64)):
+
+    def __init__(self, state_dim, action_dim, hidden_dim=32, initial_log_std=0.):
         super(ActorNet, self).__init__()
+        
+        self.mean_net = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, action_dim),
+        )
+        
+        self.log_std = nn.Parameter(torch.tensor([initial_log_std]))
+
+    def forward(self, state):
+        mu = self.mean_net(state)
+        return mu, self.log_std
+
+    def get_action(self, state, action=None, exploitation=False):
+        mean, log_std = self.forward(state)
+        if exploitation: return mean.detach()
+        policy = torch.distributions.Normal(mean, log_std.exp())
+        if action is None:
+            action = policy.rsample()
+            #if abs(state[0])<0.01: print(mean, log_std.exp(), action)
+        return action.detach(), policy.log_prob(action), policy.entropy()
+
+    def save(self, filename='actor.pth'):
+        torch.save(self.state_dict(), filename)
+
+
+class CriticNet(nn.Module):
+    def __init__(self, state_dim, hidden_size):
+        super(CriticNet, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim, hidden_size),
+            nn.Tanh(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.Tanh(),
+            nn.Linear(hidden_size, 1)
+        )
+
+    def forward(self, state):
+        return self.net(state)
+
+    def save(self, filename='critic.pth'):
+        torch.save(self.state_dict(), filename)
+
+
+class ActorNet2(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=(64, 64)):
+        super(ActorNet2, self).__init__()
         
         self.mean_net = nn.Sequential(
             nn.Linear(state_dim, hidden_dim[0]),
