6f461cc96c6dfebefd40586512bb2ed95bd0c5cc
nauqs <aquindsa10@alumnes.ub.edu>
2023-06-03 09:22:12 +0200

    Add train script

---
+++ first-steps/mini-hack/train.sh
@@ -0,0 +1,21 @@
+#$ -l tmem=8G
+#$ -l h_vmem=8G
+#$ -l h_rt=0:19:00
+#$ -l gpu=true
+
+### to exclude small gpus:
+### gpu_type=!(gtx1080ti|titanx)
+### gpu_type=!(gtx1080ti|rtx2080ti|titanxp|titanx)
+
+#$ -S /bin/bash
+#$ -wd /cluster/project2/tithonus/
+#$ -j y # merge stdout and stderr
+#$ -N job_name
+
+cd /cluster/project2/tithonus/msc-project/first-steps/mini-hack
+free -g
+echo "Assigned device: $CUDA_VISIBLE_DEVICES"
+nvidia-smi
+
+mkdir -p logs
+/cluster/project2/tithonus/anaconda3/bin/conda run -n msc-project python -u train.py &>> logs/logs.txt
