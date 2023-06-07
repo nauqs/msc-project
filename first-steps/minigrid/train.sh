24ca72cd6931aee49be98c7af997beb053af2da5
nauqs <aquindsa10@alumnes.ub.edu>
2023-06-07 15:45:47 +0200

    Add timestamp to train script

--- first-steps/minigrid/train.sh
+++ first-steps/minigrid/train.sh
@@ -1,5 +1,5 @@
-#$ -l tmem=8G
-#$ -l h_vmem=8G
+#$ -l tmem=16G
+#$ -l h_vmem=16G
 #$ -l h_rt=1:59:00
 #$ -l gpu=true
 
@@ -10,12 +10,14 @@
 #$ -S /bin/bash
 #$ -wd /cluster/project2/tithonus/
 #$ -j y # merge stdout and stderr
-#$ -N job_name
+#$ -N train-minigrid
 
 cd /cluster/project2/tithonus/msc-project/first-steps/minigrid
 free -g
 echo "Assigned device: $CUDA_VISIBLE_DEVICES"
 
+timestamp=$(date +"%m%d_%H%M%S")
+
 mkdir -p logs
 echo "Running train.py"
-/cluster/project2/tithonus/anaconda3/bin/conda run -n minigrid python -u python train.py --env_size 8x8  --max_timesteps 1000000 &>> logs/logs.txt
+/cluster/project2/tithonus/anaconda3/bin/conda run -n minigrid python -u train.py --env_size 8x8  --max_timesteps 1000000 --timestamp "$timestamp" &>> "logs/logs_$timestamp.txt"
