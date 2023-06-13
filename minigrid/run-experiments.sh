069dff6ebced22d3e3bab3b53e64c7c511846a12
nauqs <arnau.quindos@gmail.com>
2023-06-13 18:09:25 +0100

    Updated cluster scripts

---
+++ minigrid/run-experiments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+resource=$1  # This should be 'cpu' or 'gpu'
+csv_file="/cluster/project2/tithonus/msc-project/minigrid/experiments.csv"
+output_directory="/cluster/project2/tithonus/logs"
+
+# Count number of lines in csv file (excluding header)
+num_tasks=$(($(wc -l < "$csv_file") - 1))
+
+# Determine the resource to be used
+if [ "$resource" = "cpu" ]; then
+    resource_options="-l tmem=16G,h_vmem=16G,h_rt=4:00:00"
+else
+    resource_options="-l tmem=16G,h_vmem=16G,h_rt=4:00:00,gpu=true"
+fi
+
+# Submit array job
+qsub -t 2-$((num_tasks + 1)) ${resource_options} -S /bin/bash -wd /cluster/project2/tithonus/ -j y -N train-minigrid -o ${output_directory} run-task.sh
+
+echo "Array job submitted with $num_tasks tasks"
+
---
+++ minigrid/run-task.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Input Parameters
+TASK_ID=${SGE_TASK_ID}
+CSV_FILE="/cluster/project2/tithonus/msc-project/minigrid/experiments.csv"
+
+echo "TASK_ID is: $TASK_ID"
+
+# Read the specific line from the csv file
+LINE=$(sed "${TASK_ID}q;d" $CSV_FILE)
+
+# Parse the configuration
+IFS=, read -r env_id fully_obs wandb total_timesteps num_envs num_steps ent_coef <<< "$LINE"
+
+# Execute the command
+echo "Executing task with parameters: env_id=$env_id, fully_obs=$fully_obs, wandb=$wandb, total_timesteps=$total_timesteps, num_envs=$num_envs, num_steps=$num_steps, ent_coef=$ent_coef"
+cd /cluster/project2/tithonus/msc-project/minigrid
+free -g
+/cluster/project2/tithonus/miniconda3/bin/conda run -n minigrid python -u train.py --env-id $env_id --fully-obs $fully_obs --wandb $wandb --total-timesteps $total_timesteps --num-envs $num_envs --num-steps $num_steps --ent-coef $ent_coef
+
