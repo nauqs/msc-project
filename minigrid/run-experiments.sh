42ec16c829d41b493aaf4a608657bf0d6b304f11
nauqs <aquindsa10@alumnes.ub.edu>
2023-06-14 12:19:33 +0100

    Update train scripts

--- minigrid/run-experiments.sh
+++ minigrid/run-experiments.sh
@@ -12,10 +12,10 @@ if [ "$resource" = "cpu" ]; then
     resource_options="-l tmem=16G,h_vmem=16G,h_rt=4:00:00"
 else
     resource_options="-l tmem=16G,h_vmem=16G,h_rt=4:00:00,gpu=true"
+    RESOURCE="gpu"
 fi
 
 # Submit array job
-qsub -t 2-$((num_tasks + 1)) ${resource_options} -S /bin/bash -wd /cluster/project2/tithonus/ -j y -N train-minigrid -o ${output_directory} run-task.sh
+qsub -t 2-$((num_tasks + 1)) ${resource_options} -S /bin/bash -v RESOURCE=${RESOURCE} -wd /cluster/project2/tithonus/ -j y -N train-minigrid -o ${output_directory} run-task.sh
 
 echo "Array job submitted with $num_tasks tasks"
-
--- minigrid/run-task.sh
+++ minigrid/run-task.sh
@@ -12,9 +12,15 @@ LINE=$(sed "${TASK_ID}q;d" $CSV_FILE)
 # Parse the configuration
 IFS=, read -r env_id fully_obs wandb total_timesteps num_envs num_steps ent_coef <<< "$LINE"
 
+# Determine the --cuda parameter based on the RESOURCE environment variable
+if [ "$RESOURCE" = "gpu" ]; then
+    cuda_option="--cuda true"
+else
+    cuda_option="--cuda false"
+fi
+
 # Execute the command
-echo "Executing task with parameters: env_id=$env_id, fully_obs=$fully_obs, wandb=$wandb, total_timesteps=$total_timesteps, num_envs=$num_envs, num_steps=$num_steps, ent_coef=$ent_coef"
+echo "Executing task with parameters: env_id=$env_id, fully_obs=$fully_obs, wandb=$wandb, total_timesteps=$total_timesteps, num_envs=$num_envs, num_steps=$num_steps, ent_coef=$ent_coef, $cuda_option"
 cd /cluster/project2/tithonus/msc-project/minigrid
 free -g
-/cluster/project2/tithonus/miniconda3/bin/conda run -n minigrid python -u train.py --env-id $env_id --fully-obs $fully_obs --wandb $wandb --total-timesteps $total_timesteps --num-envs $num_envs --num-steps $num_steps --ent-coef $ent_coef
-
+/cluster/project2/tithonus/miniconda3/bin/conda run -n minigrid python -u train.py --env-id $env_id --fully-obs $fully_obs --wandb $wandb --total-timesteps $total_timesteps --num-envs $num_envs --num-steps $num_steps --ent-coef $ent_coef $cuda_option
